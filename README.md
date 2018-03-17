6de74a2fd4c1316968cf0a782fb6f54a107b7ec2
User00000000 <sclearovdan@gmail.com>
2018-03-17 14:05:25 +0200

    Added the script and updated the README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# automating-competitive-programming
-Script for fetching and checking problems from various online judges
+# Automated Competitive Programming
+Script for fetching and checking problems from various online judges.
+
+## TODO
+- [ ] Whole contest fetching
+- [ ] Refactor code
+- [ ] Add more online judges support
+- [ ] Make an example
---
+++ default.cpp
@@ -0,0 +1,21 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define int long long
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+ 
+const int inf = 1e18;
+const int mod = 1e9 + 7;
+const int maxn = 6e6 + 6;
+const double eps = 1e-9;
+const pair<int, int> dir[] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {-1, 1}, {-1, -1}, {1, 1}, {1, -1}};
+template <typename T> T max(T a, T b, T c) { return max(a, max(b, c)); }
+template <typename T> T min(T a, T b, T c) { return min(a, min(b, c)); }
+
+int32_t main(void) { _
+	
+}
